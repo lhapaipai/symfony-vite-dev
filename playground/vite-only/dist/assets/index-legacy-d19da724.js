bf24d34d40b6804e5932e67f1eb20e100afc6fb7
Hugues Tavernier <hugues.tavernier@protonmail.com>
2023-06-20 18:36:14 +0200

    add vite-only playground

---
+++ playground/vite-only/dist/assets/index-legacy-d19da724.js
@@ -0,0 +1,50 @@
+;
+(function () {
+  System.register([], function (exports, module) {
+    'use strict';
+
+    return {
+      execute: function () {
+        false && function polyfill() {
+          const relList = document.createElement('link').relList;
+          if (relList && relList.supports && relList.supports('modulepreload')) {
+            return;
+          }
+          for (const link of document.querySelectorAll('link[rel="modulepreload"]')) {
+            processPreload(link);
+          }
+          new MutationObserver(mutations => {
+            for (const mutation of mutations) {
+              if (mutation.type !== 'childList') {
+                continue;
+              }
+              for (const node of mutation.addedNodes) {
+                if (node.tagName === 'LINK' && node.rel === 'modulepreload') processPreload(node);
+              }
+            }
+          }).observe(document, {
+            childList: true,
+            subtree: true
+          });
+          function getFetchOpts(link) {
+            const fetchOpts = {};
+            if (link.integrity) fetchOpts.integrity = link.integrity;
+            if (link.referrerPolicy) fetchOpts.referrerPolicy = link.referrerPolicy;
+            if (link.crossOrigin === 'use-credentials') fetchOpts.credentials = 'include';else if (link.crossOrigin === 'anonymous') fetchOpts.credentials = 'omit';else fetchOpts.credentials = 'same-origin';
+            return fetchOpts;
+          }
+          function processPreload(link) {
+            if (link.ep)
+              // ep marker = processed
+              return;
+            link.ep = true;
+            // prepopulate the load record
+            const fetchOpts = getFetchOpts(link);
+            fetch(link.href, fetchOpts);
+          }
+        }();
+        console.log('welcome.js !');
+      }
+    };
+  });
+})();
---
+++ playground/vite-only/vite.config.js
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path';
+
+import legacy from '@vitejs/plugin-legacy';
+
+import vue from "@vitejs/plugin-vue";
+// import Inspect from 'vite-plugin-inspect'
+
+export default defineConfig({
+  plugins: [
+    // Inspect({}),
+    vue(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    }),
+  ],
+
+  publicDir: false,
+
+  build: {
+    manifest: true,
+    minify: false,
+  },
+
+  server: {
+    // port: 3000,
+    // host: '0.0.0.0',
+    // strictPort: true,
+    // host: '127.0.0.1',
+    // port: 23456, // local port listened by Vite and proxied by NGINX
+    // origin: 'https://example.com', // HTTPS & WSS served by NGINX
+  },
+
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, 'assets'),
+    }
+  }
+});
