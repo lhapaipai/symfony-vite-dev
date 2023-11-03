5dd96d713e6dcc1b2ab8b6d529e02424872a39c8
Hugues Tavernier <hugues.tavernier@protonmail.com>
2023-11-03 09:27:04 +0100

    add github issue templates

--- split.sh
+++ split.sh
@@ -2,5 +2,13 @@
 
 set -e
 
-git subtree push --prefix=src/vite-bundle git@github.com:lhapaipai/vite-bundle.git main
-git subtree push --prefix=src/vite-plugin-symfony git@github.com:lhapaipai/vite-plugin-symfony.git main
+# Make sure the working directory is clear.
+if [[ ! -z "$(git status --porcelain)" ]]
+then
+    echo "Your working directory is dirty. Did you forget to commit your changes?"
+
+    exit 1
+fi
+
+git subtree push --prefix=src/vite-bundle git@github.com:lhapaipai/vite-bundle.git feature/tests
+git subtree push --prefix=src/vite-plugin-symfony git@github.com:lhapaipai/vite-plugin-symfony.git feature/v6
