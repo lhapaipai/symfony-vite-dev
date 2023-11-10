dece395f712efcadabb8d2f650ac9cd538548566
Hugues Tavernier <hugues.tavernier@protonmail.com>
2023-11-10 10:04:08 +0100

    stimulus add lazy custom controllers

--- playground/stimulus/assets/bootstrap.js
+++ playground/stimulus/assets/bootstrap.js
@@ -1,7 +1,7 @@
-import { startStimulusApp, registerVueControllerComponents} from "vite-plugin-symfony/stimulus-helpers"
+import { startStimulusApp, registerVueControllerComponents} from "vite-plugin-symfony/stimulus/helpers"
 
 registerVueControllerComponents(import.meta.glob('./vue/controllers/**/*.vue'))
 
-const app = startStimulusApp(import.meta.glob('./controllers/*_controller.js'));
+const app = startStimulusApp(import.meta.glob('./controllers/*_(lazy)\?controller.[jt]s(x)\?'));
 
 
