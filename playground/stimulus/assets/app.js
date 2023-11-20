4555cb462edd56071f5a72e64d2985fb4e8c98ed
Hugues Tavernier <hugues.tavernier@protonmail.com>
2023-11-20 23:27:09 +0100

    stimulus hmr add tests

--- playground/stimulus/assets/app.js
+++ playground/stimulus/assets/app.js
@@ -1,6 +1,3 @@
-// import { registerVueControllerComponents } from '@symfony/ux-vue';
-// import { registerSvelteControllerComponents } from '@symfony/ux-svelte';
-// import { registerReactControllerComponents } from '@symfony/ux-react';
 import './bootstrap.js';
 
 function refreshStickStatus() {
@@ -11,14 +8,5 @@ let $nav = document.querySelector("#nav");
 if ($nav) {
   window.addEventListener("scroll", refreshStickStatus);
   refreshStickStatus();
-
-
-
 }
 
-// if (import.meta.hot) {
-//   import.meta.hot.accept((mod, ctx) => {
-//     import.meta.hot.send('restart')
-//     console.log(mod, ctx)
-//   })
-// }
