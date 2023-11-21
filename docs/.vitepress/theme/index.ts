e7a77a28f5e2329c6b3063cc3f5aa24249eaf212
Hugues Tavernier <hugues.tavernier@protonmail.com>
2023-11-21 12:51:10 +0100

    add custom print urls

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,5 @@
 // https://vitepress.dev/guide/custom-theme
+// list of components : https://github.com/vuejs/vitepress/tree/main/src/client/theme-default/components
 import { h } from 'vue';
 import Theme from 'vitepress/theme';
 import './style.css';
