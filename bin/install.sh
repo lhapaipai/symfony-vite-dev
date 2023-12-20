6413aa170ead3e7958ba63d15834d733f2f256e4
Hugues Tavernier <hugues.tavernier@protonmail.com>
2023-12-20 23:42:59 +0100

    add stimulus composer dependencies to global install to prevent pnpm recursive install to fail

--- bin/install.sh
+++ bin/install.sh
@@ -6,11 +6,13 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
 PLAYGROUNDS_DIR="$PROJECT_DIR/playground"
 
+cd "$PLAYGROUNDS_DIR/stimulus"
+symfony composer install
 
 cd "$PROJECT_DIR"
 # we need this because package.json contain reference
 # of vendor/symfony/ux-* files
-composer install --no-autoloader
+symfony composer install --no-autoloader
 pnpm install
 
 cd "$PROJECT_DIR/src/vite-bundle"
